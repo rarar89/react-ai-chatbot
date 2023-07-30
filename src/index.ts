0093ca89a2a7624724048388361cda8c493c5f0e
raiviss <rstudens@gmail.com>
2023-07-30 13:29:36 +0300

    readme update

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 export { ChatBotProvider } from './context/ChatBotProvider';
 export { default as useChatBot } from './hooks/useChatBot';
-export { default as useIncoming } from './hooks/usePending';
+export { default as usePending } from './hooks/usePending';
 
 import './demo';
