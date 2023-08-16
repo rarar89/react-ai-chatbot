4869f23a1020bf49cac975cd369e41689eabba9c
raiviss <rstudens@gmail.com>
2023-08-16 22:20:00 +0300

    demo removal

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,3 @@
 export { ChatBotProvider } from './context/ChatBotProvider';
 export { default as useChatBot } from './hooks/useChatBot';
 export { default as usePending } from './hooks/usePending';
-
-import './demo';
